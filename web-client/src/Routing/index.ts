a0ce9a11970231747a8be83d2babd0fed6fbc2eb
Kanmii <maneptha@gmail.com>
2018-11-27 18:39:59 +0100

    setTitle uses document.title and changes order of title.

--- web-client/src/Routing/index.ts
+++ web-client/src/Routing/index.ts
@@ -2,14 +2,6 @@ export const ROOT_URL = "/";
 export const LOGIN_URL = "/login";
 export const SIGN_UP_URL = "/sign-up";
 
-let titleEl = document.getElementById("ebnis-title");
-
 export const setTitle = (title?: string) => {
-  if (!titleEl) {
-    titleEl = document.getElementById("ebnis-title");
-  }
-
-  if (titleEl) {
-    titleEl.innerText = title ? `Ebnis | ${title}` : "Ebnis";
-  }
+  document.title = title ? `${title} | Ebnis` : "Ebnis";
 };
