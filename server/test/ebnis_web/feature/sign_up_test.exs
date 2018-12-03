d4acdf24e8f6a201e19354e127dafad5b368c517
Kanmii <maneptha@gmail.com>
2018-12-03 07:27:59 +0100

    Feature test file broken up into login and sign up files.
    I'm not sure why they should belong to different files - but I guess
    we will see.

--- server/test/ebnis_web/feature/sign_up_test.exs
+++ server/test/ebnis_web/feature/sign_up_test.exs
@@ -1,4 +1,4 @@
-defmodule EbnisWeb.FeatureCase do
+defmodule EbnisWeb.Feature.SignUpTest do
   use Ebnis.HoundCase, async: false
 
   import Mox
@@ -45,27 +45,5 @@ defmodule EbnisWeb.FeatureCase do
     assert %User{email: ^email, id: _} = Repo.get_by(User, email: email)
   end
 
-  # @tag :no_headless
-  @tag :log_in_feature
-  test "Login" do
-    # Given a user exists in the system
-    params = RegFactory.params()
-    RegFactory.insert(params)
 
-    # When a user reaches our page
-    path() |> navigate_to()
-
-    # She sees the login text in the page title
-    assert page_title() =~ "Log in"
-
-    # When she fills in her email and password
-    fill_field({:name, "email"}, params.email)
-    fill_field({:name, "password"}, params.password)
-
-    # And she submits the form
-    submit_element({:name, "login-submit"})
-
-    # She is redirected to home page
-    assert retries(true, fn -> page_title() =~ "Home" end, 1_000)
-  end
 end
