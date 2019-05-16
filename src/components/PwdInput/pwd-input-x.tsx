f8e11ec7dbf36f0904345d1c3141815a3754b60d
Adekanmi Ademiiju <maneptha@gmail.com>
2019-05-16 16:28:49 +0200

    PasswordInput Component - reduce code complexity.

--- src/components/PwdInput/pwd-input-x.tsx
+++ src/components/PwdInput/pwd-input-x.tsx
@@ -3,64 +3,64 @@ import React from "react";
 import { Props, PwdInputActionTypes } from "./pwd-input";
 import { Form, Input, Icon } from "semantic-ui-react";
 
-export const PwdInput = React.memo(pwdInput, xx);
+export const PwdInput = React.memo(
+  function PwdInputComponent<TFormValues>(props: Props<TFormValues>) {
+    const { field, pwdType = "password", dispatch } = props;
+    const id = makeId(field.name);
+
+    return (
+      <Form.Field>
+        <label htmlFor={id}>Password</label>
+
+        <Input icon={true} placeholder="" data-testid={id}>
+          <input {...field} type={pwdType} autoComplete="off" id={id} />
+
+          {pwdType === "password" && field.value && (
+            <Icon
+              name="eye"
+              className="link"
+              data-testid="password-unmask"
+              onClick={() =>
+                dispatch({
+                  payload: "text",
+                  type: PwdInputActionTypes.SET_PWD_TYPE
+                })
+              }
+            />
+          )}
+
+          {pwdType === "text" && field.value && (
+            <Icon
+              name="eye slash"
+              className="link"
+              data-testid="password-mask"
+              onClick={() =>
+                dispatch({
+                  payload: "password",
+                  type: PwdInputActionTypes.SET_PWD_TYPE
+                })
+              }
+            />
+          )}
+        </Input>
+      </Form.Field>
+    );
+  },
+
+  function PwdInputDiff(p, n) {
+    if (p.pwdType !== n.pwdType) {
+      return false;
+    }
+
+    if (p.field.value !== n.field.value) {
+      return false;
+    }
+
+    return true;
+  }
+);
 
 export default PwdInput;
-
-function xx<T>(p: Props<T>, n: Props<T>) {
-  if (p.pwdType !== n.pwdType) {
-    return false;
-  }
-
-  if (p.field.value !== n.field.value) {
-    return false;
-  }
-
-  return true;
-}
-
-function pwdInput<TFormValues>(props: Props<TFormValues>) {
-  const { field, pwdType = "password", dispatch } = props;
-  const id = makeId(field.name);
-
-  return (
-    <Form.Field>
-      <label htmlFor={id}>Password</label>
-
-      <Input icon={true} placeholder="" data-testid={id}>
-        <input {...field} type={pwdType} autoComplete="off" id={id} />
-
-        {pwdType === "password" && field.value && (
-          <Icon
-            name="eye"
-            className="link"
-            data-testid="password-unmask"
-            onClick={() =>
-              dispatch({
-                payload: "text",
-                type: PwdInputActionTypes.SET_PWD_TYPE
-              })
-            }
-          />
-        )}
-
-        {pwdType === "text" && field.value && (
-          <Icon
-            name="eye slash"
-            className="link"
-            data-testid="password-mask"
-            onClick={() =>
-              dispatch({
-                payload: "password",
-                type: PwdInputActionTypes.SET_PWD_TYPE
-              })
-            }
-          />
-        )}
-      </Input>
-    </Form.Field>
-  );
-}
 
 export function makeId(name: string) {
   return `pwd-input-${name}`;
