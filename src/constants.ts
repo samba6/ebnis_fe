d46389e438d6a5ef918df6fc639f0e4296c3f2a9
Adekanmi Ademiiju <maneptha@gmail.com>
2019-06-10 13:29:16 +0200

    Bumped offline schema version.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = "ebnis-token-key";
-export const SCHEMA_VERSION = "1.2"; // Must be a string.
+export const SCHEMA_VERSION = "1.02"; // Must be a string.
 export const SCHEMA_VERSION_KEY = "ebnis-apollo-schema-version";
 export const SCHEMA_KEY = "ebnis-apollo-cache-persist";
 export const SITE_TITLE = "Ebnis";
