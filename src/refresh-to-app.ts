080aa6a3d66284efd7bf3c03983cf822dec9c59b
Adekanmi Ademiiju <maneptha@gmail.com>
2019-06-12 22:38:42 +0200

    Redirect user has soon as they are logged in.
    No need to wait for the cache to be written to disk before redirecting
    user since we are now waiting for the cache to persist before redering
    components.

--- src/refresh-to-app.ts
+++ src/refresh-to-app.ts
@@ -1,45 +1,5 @@
 import { EXPERIENCES_URL } from "./routes";
-import { SCHEMA_KEY } from "./constants/apollo-schema";
 
-/**
- * Wait for a newly created/logged in user to be written to local storage before
- * redirecting user.  On slow systems, we wait for up to 10secs!!!!!!!!!
- */
 export function refreshToHome() {
-  function getUser() {
-    const data = localStorage.getItem(SCHEMA_KEY);
-
-    if (!data) {
-      return null;
-    }
-
-    try {
-      const { ROOT_QUERY } = JSON.parse(data);
-
-      if (!ROOT_QUERY) {
-        return null;
-      }
-
-      return ROOT_QUERY.user;
-    } catch {
-      return null;
-    }
-  }
-
-  if (getUser()) {
-    window.location.href = EXPERIENCES_URL;
-    return;
-  }
-
-  let intervalId: NodeJS.Timeout;
-  let counter = 0;
-
-  intervalId = setInterval(() => {
-    if (getUser() || counter === 1000) {
-      clearInterval(intervalId);
-      window.location.href = EXPERIENCES_URL;
-    }
-
-    counter++;
-  }, 10);
+  window.location.href = EXPERIENCES_URL;
 }
