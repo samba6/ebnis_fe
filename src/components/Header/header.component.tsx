f91194a8cc3db1b246c87fdf657dd7e362484ffb
Kanmii <maneptha@gmail.com>
2019-09-27 19:22:43 +0200

    Removed logo from main app.
    Because I found that other mobile apps do not usually show the
    logo.

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -4,16 +4,12 @@ import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 import makeClassnames from "classnames";
 import { Link } from "../Link";
 import "./header.styles.scss";
-import { EXPERIENCES_URL, ROOT_URL } from "../../routes";
 import { LayoutContextHeader, LocationContext } from "../Layout/layout.utils";
 import {
   UPLOAD_UNSAVED_PREVIEW_URL,
   UPLOAD_UNSAVED_URL_START,
 } from "../../constants/upload-unsaved-routes";
-import { useUser } from "../use-user";
 import { SetStateAction, PropsWithChildren } from "react";
-import { LogoImageQuery_file_childImageSharp_fixed } from "../../graphql/gatsby-types/LogoImageQuery";
-import { useLogo } from "./header.injectables";
 
 export const Header = (props: Props) => {
   const {
@@ -25,19 +21,8 @@ export const Header = (props: Props) => {
     className = "",
   } = props;
 
-  const user = useUser();
-  const logoAttrs = useLogo();
-  const { navigate, pathname } = useContext(LocationContext);
-
-  const isHome = pathname === EXPERIENCES_URL || pathname === ROOT_URL;
-
+  const { pathname } = useContext(LocationContext);
   const { unsavedCount, hasConnection } = useContext(LayoutContextHeader);
-
-  const asUrlProps = isHome
-    ? {}
-    : {
-        onClick: () => navigate(user ? EXPERIENCES_URL : ROOT_URL),
-      };
 
   return (
     <header
@@ -49,23 +34,17 @@ export const Header = (props: Props) => {
       })}
     >
       <Menu secondary={true}>
-        <style>{getLogoStyle(logoAttrs)}</style>
-
-        <Menu.Item
-          id="header-logo-container"
-          className={makeClassnames({
-            "logo-container": true,
-            "with-pointer": !isHome,
-            "center-children": !sidebar,
-          })}
-          {...asUrlProps}
-        >
-          <div id="components-header-logo" />
-        </Menu.Item>
-
-        {sidebar && (
+        {unsavedCount > 0 && !pathname.includes(UPLOAD_UNSAVED_URL_START) ? (
+          <Link
+            to={UPLOAD_UNSAVED_PREVIEW_URL}
+            id="header-unsaved-count-label"
+            className="unsaved-count-label"
+          >
+            {unsavedCount}
+          </Link>
+        ) : sidebar ? (
           <Menu.Item
-            position="right"
+            position="left"
             className="sidebar-trigger"
             onClick={() => toggleShowSidebar && toggleShowSidebar(!show)}
             id="header-sidebar-trigger"
@@ -80,18 +59,8 @@ export const Header = (props: Props) => {
               <Icon id="header-show-sidebar-icon" name="content" />
             )}
           </Menu.Item>
-        )}
+        ) : null}
       </Menu>
-
-      {unsavedCount > 0 && !pathname.includes(UPLOAD_UNSAVED_URL_START) && (
-        <Link
-          to={UPLOAD_UNSAVED_PREVIEW_URL}
-          id="header-unsaved-count-label"
-          className="unsaved-count-label"
-        >
-          {unsavedCount}
-        </Link>
-      )}
 
       {(title || children) &&
         (title ? (
@@ -110,11 +79,6 @@ export const Header = (props: Props) => {
     </header>
   );
 };
-
-function getLogoStyle(logoAttrs: LogoImageQuery_file_childImageSharp_fixed) {
-  const { src, width, height } = logoAttrs;
-  return `#components-header-logo{ background: url(${src}) no-repeat 0 !important; background-size: ${width}px ${height}px !important; min-width: ${width}px; min-height: ${height}px;}`;
-}
 
 ////////////////////////// TYPES ////////////////////////////
 
