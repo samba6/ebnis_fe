b88f0ff35fdbf0489c5e8be80a6a565a2c60395a
Kanmii <maneptha@gmail.com>
2020-01-12 22:22:47 +0100

    updated apollo schema version

--- src/constants/apollo-schema.ts
+++ src/constants/apollo-schema.ts
@@ -1,3 +1,3 @@
-export const SCHEMA_VERSION = "1.10"; // Must be a string.
+export const SCHEMA_VERSION = "1.11"; // Must be a string.
 export const SCHEMA_VERSION_KEY = "GQ7Fl9vKDh6cR8aiWAXnOMkiGCq5iL61PZZo";
 export const SCHEMA_KEY = "3oujRi00WHDSC42NhbS3cgREmvH6";
