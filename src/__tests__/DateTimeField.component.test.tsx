56cdaeaf39911d101b1369278bfdb667ff9a9824
Kanmii <maneptha@gmail.com>
2020-03-20 09:57:37 +0100

    wip

--- src/__tests__/DateTimeField.component.test.tsx
+++ src/__tests__/DateTimeField.component.test.tsx
@@ -38,22 +38,25 @@ it("renders ", () => {
    */
   const [, , , h, mi] = formatDate(currentDate, "yyyy MMM d HH mm").split(" ");
 
-  const $hr = document.getElementById(
+  const hourFieldDom = document.getElementById(
     `datetime-hour-field-f.hr`,
   ) as HTMLDivElement;
 
   expect(
-    ($hr.getElementsByClassName(selectedItemClassName)[0] as any).textContent,
+    (hourFieldDom
+      .getElementsByClassName(selectedItemClassName)
+      .item(0) as HTMLElement).textContent,
   ).toEqual(h);
-  return;
 
-  const $min = document.getElementById(
+  const minuteFieldDom = document.getElementById(
     `datetime-minute-field-f.min`,
   ) as HTMLDivElement;
 
-  expect(($min.getElementsByClassName("active")[0] as any).textContent).toEqual(
-    mi,
-  );
+  expect(
+    (minuteFieldDom
+      .getElementsByClassName(selectedItemClassName)
+      .item(0) as HTMLElement).textContent,
+  ).toEqual(mi);
 
   /**
    * When we change the date to two years, 3 months, 5 days, 2 hours and
@@ -66,43 +69,35 @@ it("renders ", () => {
 
   const [, , , h1, mi1] = formatDate(newDate, "yyyy MMM d HH mm").split(" ");
 
-  (document.getElementsByClassName(
-    `js-datetime-field-input-hour-${h1}`,
-  )[0] as any).click();
+  (document
+    .getElementsByClassName(`js-datetime-field-input-hour-${h1}`)
+    .item(0) as HTMLElement).click();
 
-  (document.getElementsByClassName(
-    `js-datetime-field-input-minute-${mi1}`,
-  )[0] as any).click();
+  (document
+    .getElementsByClassName(`js-datetime-field-input-minute-${mi1}`)
+    .item(0) as HTMLElement).click();
 
   /**
    * Then the new date should have been set
    */
+  const calls = mockSetValue.mock.calls;
 
-  const [c01, c02] = mockSetValue.mock.calls[0];
-  expect(c01).toEqual("f");
-  expect(c02.getFullYear()).toBe(currentDate.getFullYear() - 2); // 2 years ago
-
-  const [c11, c12] = mockSetValue.mock.calls[1];
-  expect(c11).toEqual("f");
-  expect(c12.getMonth()).toBe(currentDate.getMonth() - 3); // 3 months ago
-
-  const [c21, c22] = mockSetValue.mock.calls[2];
-  expect(c21).toEqual("f");
-  expect(c22.getDate()).toBe(currentDate.getDate() - 5); // 5 days ago
+  const [c01, c02] = calls[0];
+  expect(c01).toEqual(name);
+  expect(c02.getHours()).toBe(currentDate.getHours() - 2); // 2 hours ago
 
   expect(
-    ($hr.getElementsByClassName("active")[0] as HTMLDivElement).textContent,
+    (hourFieldDom
+      .getElementsByClassName(selectedItemClassName)
+      .item(0) as HTMLDivElement).textContent,
   ).toEqual(h1);
 
-  const [c31, c32] = mockSetValue.mock.calls[3];
-  expect(c31).toEqual("f");
-  expect(c32.getHours()).toBe(currentDate.getHours() - 2); // 2 hours ago
+  const [, c1] = calls[1];
+  expect(c1.getMinutes()).toBe(currentDate.getMinutes() - 4); // 4 minutes ago
 
   expect(
-    ($min.getElementsByClassName("active")[0] as HTMLDivElement).textContent,
+    (minuteFieldDom
+      .getElementsByClassName(selectedItemClassName)
+      .item(0) as HTMLDivElement).textContent,
   ).toEqual(mi1);
-
-  const [c41, c42] = mockSetValue.mock.calls[4];
-  expect(c41).toEqual("f");
-  expect(c42.getMinutes()).toBe(currentDate.getMinutes() - 4); // 4 minutes ago
 });
